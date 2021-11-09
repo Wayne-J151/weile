4e83a3353355836a49d633695efafc57a6032763
Wayne-J151 <73774260+Wayne-J151@users.noreply.github.com>
2021-11-09 12:52:32 +0800

    Update weile.py

--- weile.py
+++ weile.py
@@ -3,3 +3,4 @@ import json
 url = 'https://wapi.weile.com/broadcast/wel-game/v2/receive/122/818/1.3.37.153.325/520500?format=json&code=1233&userid=165143389'
 r = requests.post(url)
 print(r.text)
+
