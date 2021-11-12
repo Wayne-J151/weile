c90a044af16a1e49104c38ed7c1e2b35115439de
Wayne-J151 <73774260+Wayne-J151@users.noreply.github.com>
2021-11-12 17:15:03 +0800

    Update weile.py

--- weile.py
+++ weile.py
@@ -1,26 +1,34 @@
 import requests
 import json
-j=0
 
-for i in range(9999):
-  i=i+1000
-  url = 'https://wapi.weile.com/broadcast/wel-game/v2/receive/122/818/1.3.37.153.325/520500?format=json&code='+str(i)+'&userid=165143389'
-  r = requests.post(url)
-  if(json.loads(r.text)["msg"]!="领取失败，请出入正确的兑换码"):
-    j=j+1
-    print(i)
-    print("成功")
-    if(j>3):
-      break
-j=0
-for i in range(9999):
-  i=i+1000
-  url = 'https://wapi.weile.com/broadcast/wel-game/v2/receive/122/818/1.3.37.153.325/520101?format=json&code='+str(i)+'&userid=108388169'
-  r = requests.post(url)
-  if(json.loads(r.text)["msg"]!="领取失败，请出入正确的兑换码"):
-    j=j+1
-    print(i)
-    print("成功")
-    if(j>3):
-      break
 
+uids=[165143389,108388169]
+
+def get_bean_from_code(uid):
+    flag = 0
+    for i in range(1000,9999):
+        url = 'https://wapi.weile.com/broadcast/wel-game/v2/receive/122/818/1.3.37.153.325/520500?format=json&code='+str(i)+'&userid='+str(uid)
+        r = requests.post(url)
+        if(json.loads(r.text)["msg"]!="领取失败，请出入正确的兑换码"):
+            flag=flag+1
+            print(i)
+            print("成功")
+        if(flag>1):
+          break
+     return(1)
+def get_bean_from_luckyturn(uid):
+    for i in range(20):
+        url = 'https://wapi.weile.com/shake/turntable/draw/122/818/1.3.38.153.326/520101?format=json&userid='+str(uid)
+        r = requests.post(url)
+        url = "https://wapi.weile.com/shake/turntable/receive/122/818/1.3.38.153.326/520101?format=json&is_double=1&userid="+str(uid)
+        r = requests.post(url)
+        if(json.loads(r.text)['msg'] == '已经达到抽奖次数'):
+            break
+     return(1)
+ 
+for uid  in uids:
+    
+    a = get_bean_from_code(uid)
+    b = get_bean_from_luckyturn(uid)
+    if(a+b==2):
+      print(uid,"is OK")
